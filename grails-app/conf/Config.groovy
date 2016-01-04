4649b96b2d7a51a66deed7aab21d6f9d5326bf5e
Meni Lubetkin <meni@3baysover.com>
2016-01-04 10:32:00 +0200

    clean config

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -91,7 +91,7 @@ environments {
     }
     production {
         grails.logging.jul.usebridge = false
-        grails.serverURL = "http:// grails-app.elasticbeanstalk.com"
+        grails.serverURL = "http:// www.example.com"
     }
 }
 
